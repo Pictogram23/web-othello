53276d868269c19c4b691eab72ad6bfd731c287b
Pictogram23 <shanchengyinger@gmail.com>
2024-03-01 10:55:18 +0900

    不要と思われるurlを削除

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("drf_social_oauth2.urls", namespace="drf")),
     path("", include("othello.urls")),
 ]
