d7b53bcf5015374b71925e743b7b39134065f03c
Pictogram23 <shanchengyinger@gmail.com>
2024-03-02 00:59:56 +0900

    eslint、prettierの導入、設定

--- frontend/app/_components/stageComponent.tsx
+++ frontend/app/_components/stageComponent.tsx
@@ -1,24 +1,24 @@
-'use client';
+'use client'
 
-import { Layer, Stage } from "react-konva";
-import Board from "./board";
-import { FC } from "react";
-import { KonvaEventObject } from "konva/lib/Node";
+import { KonvaEventObject } from 'konva/lib/Node'
+import { FC } from 'react'
+import { Layer, Stage } from 'react-konva'
+import Board from './board'
 
 const StageComponent: FC = () => {
-    const boardClicked = (e:KonvaEventObject<MouseEvent>) => {
-        let x, y;
-        x = Math.floor(e.evt.offsetX / 50);
-        y = Math.floor(e.evt.offsetY / 50);
-        console.log([x, y]);
-    } 
-    return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-            <Layer>
-                <Board onClick={boardClicked} />
-            </Layer>
-        </Stage>
-    )
+  const boardClicked = (e: KonvaEventObject<MouseEvent>) => {
+    let x, y
+    x = Math.floor(e.evt.offsetX / 50)
+    y = Math.floor(e.evt.offsetY / 50)
+    console.log([x, y])
+  }
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Board onClick={boardClicked} />
+      </Layer>
+    </Stage>
+  )
 }
 
-export default StageComponent;
+export default StageComponent
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,30 @@
 'use client'
 
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 import * as http from 'http'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
 
 type User = {
-  email: string,
-  name: string,
+  email: string
+  name: string
 }
 
-const StageComponent = dynamic(() => import('./_components/stageComponent'), {ssr: false})
+const StageComponent = dynamic(() => import('./_components/stageComponent'), { ssr: false })
 export default function Home() {
-  const [ users, setUsers ] = useState<User[] | null>(null)
-  const [ localUser, setLocalUser ] = useState<User | null>(null)
-  const [ logined, setLogined ] = useState<boolean>(false)
+  const [users, setUsers] = useState<User[] | null>(null)
+  const [localUser, setLocalUser] = useState<User | null>(null)
+  const [logined, setLogined] = useState<boolean>(false)
 
   const updateLocalUser = async (_users: User[]) => {
-    const _localUser: User = JSON.parse(localStorage.getItem("key") as string);
+    const _localUser: User = JSON.parse(localStorage.getItem('key') as string)
     _users?.map((_user) => {
-      if(_user.email == _localUser.email) {
-        setLogined(true);
+      if (_user.email == _localUser.email) {
+        setLogined(true)
       }
     })
-    setLocalUser(_localUser);
+    setLocalUser(_localUser)
   }
 
   useEffect(() => {
@@ -32,10 +32,10 @@ export default function Home() {
     const getUsers = async () => {
       const res = await GET()
       res.map((data: any) => {
-        const _user: User = {email: data.email, name: data.name}
-        _users.push(_user);
+        const _user: User = { email: data.email, name: data.name }
+        _users.push(_user)
       })
-      setUsers(_users);
+      setUsers(_users)
       await updateLocalUser(_users)
     }
     getUsers()
@@ -43,63 +43,67 @@ export default function Home() {
 
   return (
     <>
-      <GoogleOAuthProvider clientId="128697940905-jtbpl7g86j7kbe9otv9lrj24tit2krqr.apps.googleusercontent.com">
-        {logined
-        ? <div>{localUser?.name}でログイン済み</div>
-        : <GoogleLogin 
-          onSuccess={credentialResponse => {
-            const decoded = jwtDecode(credentialResponse.credential as string)
-            const newUser: User = {
-              email: decoded.email,
-              name: decoded.given_name,
-            }
-            var skip = false;
-            users?.forEach(user => {
-              if (user.email == newUser.email) {
-                skip = true;
+      <GoogleOAuthProvider clientId='128697940905-jtbpl7g86j7kbe9otv9lrj24tit2krqr.apps.googleusercontent.com'>
+        {logined ? (
+          <div>{localUser?.name}でログイン済み</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              const decoded = jwtDecode(credentialResponse.credential as string)
+              const newUser: User = {
+                email: decoded.email,
+                name: decoded.given_name,
               }
-            })
-            if(skip == false) {
-              POST(newUser);
-            }
-            window.localStorage.clear()
-            window.localStorage.setItem('key', JSON.stringify(newUser));
-            if(users !== null) {
-              updateLocalUser(users)
-            }
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />}
+              var skip = false
+              users?.forEach((user) => {
+                if (user.email == newUser.email) {
+                  skip = true
+                }
+              })
+              if (skip == false) {
+                POST(newUser)
+              }
+              window.localStorage.clear()
+              window.localStorage.setItem('key', JSON.stringify(newUser))
+              if (users !== null) {
+                updateLocalUser(users)
+              }
+            }}
+            onError={() => {
+              console.log('Login Failed')
+            }}
+          />
+        )}
       </GoogleOAuthProvider>
       <StageComponent />
       <div>
-        {
-          users?.map((item) => {
-            return <li>{item.name} {item.email}</li>;
-          })
-        }
+        {users?.map((item) => {
+          return (
+            <li key={item.email}>
+              {item.name} {item.email}
+            </li>
+          )
+        })}
       </div>
     </>
-  );
+  )
 }
 
 export const GET = async () => {
-  const res = await fetch('http://localhost:8000/users/');
-  const users = await res.json();
+  const res = await fetch('http://localhost:8000/users/')
+  const users = await res.json()
   return users
 }
 
 const options = {
-  method: "POST",
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
   host: 'localhost',
   port: 8000,
   path: '/users/',
-};
+}
 
 export const POST = async (user: User) => {
   const request = http.request(options, (res) => {
